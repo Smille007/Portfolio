e2eb8bb6dd7919fc2ef898486a2abff652f40396
Smille007 <smille007@gmai.com>
2024-11-23 13:34:58 -0500

    symbols for menu

--- touchless/src/App.jsx
+++ touchless/src/App.jsx
@@ -6,20 +6,9 @@ import Header from "./components/Header"
 
 const App = () =>{
   return(
-   <header className='fixed top-0 left -0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0'>
-<div className="max-w-screen-2xl w-full mx-auto px-4">
-<h1>
-  <a href="/" className="logo">
-  <img src="/public/images/logo.svg"
-  width={40} 
-  height={40}alt="Touchless" />
-  </a>
-</h1>
+    <Header/>
 
 
-
-  </div>
-    </header>
   )
 }
 
--- touchless/src/components/Header.jsx
+++ touchless/src/components/Header.jsx
@@ -6,8 +6,26 @@
 
 const Header = () => {
   return (
-    <div>Header</div>
+ <header className="fixed top-0 left -0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+    <div className="max-w-screen-2xl w-full mx-auto px-4">
+    <h1>
+      <a href="/" className="logo">
+      <img src="/public/images/logo.svg"
+      width={40} 
+      height={40}alt="Touchless" />
+<h1></h1>
+      </a>
+  </h1>
+  <div className=" ">
+    <button className='' onClick={null}>
+        <span className="material-symbols-rounded">
+        menu</span>
+    
+    </button>
+    Navbar
+  </div>
+</div>
+  </header>
   )
-}
-
+}  
 export default Header
