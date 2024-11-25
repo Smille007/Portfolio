796c3916ef58afc0e6ba1642959c6d48031f9ad2
Smille007 <smille007@gmai.com>
2024-11-25 13:27:03 -0500

    header ready

--- touchless/src/components/Header.jsx
+++ touchless/src/components/Header.jsx
@@ -6,29 +6,36 @@
 
 const Header = () => {
   return (
- <header className="fixed top-0 left -0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-    <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center">
-    <h1>
-      <a href="/" className="logo">
-      <img src="images/logo.svg"
-      width={40} 
-      height={40}alt="Touchless" />
-      </a>
-  </h1>
-  <div className="">
-    <button
-     className="" 
-    onClick={null}>
-        <span className="material-symbols-rounded">
- menu
-        </span>
-        Navbar
-    </button>
-  </div>
-  <a href='#contact' className=''></a>
-  Contact us
-</div>
-  </header>
-  )
-}  
-export default Header
+    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+      <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[3fr,1fr,3fr]">
+        {/* Logo */}
+        <h1>
+          <a href="/" className="logo">
+            <img
+              src="images/logo.svg"
+              width={40}
+              height={40}
+              alt="Touchless"
+            />
+          </a>
+        </h1>
+        
+        {/* Navbar Button (Hidden on medium and larger screens) */}
+        <div className="relative md: justify-self-center">
+          <button className="menu-btn md:hidden" onClick={null}>
+            <span className="material-symbols-rounded">menu</span>
+          </button>
+          Navbar
+        </div>
+
+        {/* Contact Us */}
+        <a href="#contact" className="text-right">
+          Contact Us
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+
