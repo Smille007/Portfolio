f262462f3cd725c8d464ed4cabd61268d779fef0
Smille007 <smille007@gmai.com>
2024-11-30 13:00:17 -0500

    button import issue

---
+++ touchless/src/components/Button.jsx
@@ -0,0 +1,89 @@
+/**
+ * @copyright 2024 Daryna Vershinina Diaz
+ * @license Apache-2.0
+ */
+
+/**
+ * Node Modules
+ */
+import PropTypes from "prop-types";
+
+/**
+ * Primary Button
+ */
+
+
+const ButtonPrimary = ({
+  href,
+  target = "_self",
+  label,
+  icon,
+  classes,
+}) => {
+  if (href) {
+    return (
+      <a href={href} target={target} className={"btn btn-primary " + classes}>
+        {label}
+        {icon ? (
+          <span className="material-symbols-rounded" aria-hidden="true">
+            {icon}
+          </span>
+        ) : undefined}
+      </a>
+    );
+  } else {
+    return (
+      <button className={"btn btn-primary " + classes}>
+        {label}
+      </button>
+    );
+  }
+};
+
+ButtonPrimary.propTypes = {
+  label: PropTypes.string.isRequired,
+  href: PropTypes.string,
+  target: PropTypes.string,
+  icon: PropTypes.string,
+  classes: PropTypes.string,
+};
+
+/**
+ * Outline Button
+ */
+const ButtonOutline = ({
+    href,
+    target = "_self",
+    label,
+    icon,
+    classes,
+  }) => {
+    if (href) {
+      return (
+        <a href={href} target={target} className={"btn btn-outline " + classes}>
+          {label}
+          {icon ? (
+            <span className="material-symbols-rounded" aria-hidden="true">
+              {icon}
+            </span>
+          ) : undefined}
+        </a>
+      );
+    } else {
+      return (
+        <button className={"btn btn-outline " + classes}>
+          {label}
+        </button>
+      );
+    }
+  };
+  
+  ButtonOutline.propTypes = {
+    label: PropTypes.string.isRequired,
+    href: PropTypes.string,
+    target: PropTypes.string,
+    icon: PropTypes.string,
+    classes: PropTypes.string,
+  };
+
+export default {ButtonPrimary, ButtonOutline}
