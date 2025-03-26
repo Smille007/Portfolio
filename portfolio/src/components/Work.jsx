eedbf610a892cb94f644d4df4034738765362d7f
Smille007 <smille007@gmai.com>
2025-03-26 15:42:02 -0400

    bugs fixed

--- portfolio/src/components/Work.jsx
+++ portfolio/src/components/Work.jsx
@@ -16,37 +16,37 @@ const works = [
       tags: ['API', 'SQL', 'Development'],
       projectLink: 'https://djwedding.netlify.app/'
     },
-    {
-      imgSrc: '/images/project-2.jpg',
-      title: 'Free stock photo app',
-      tags: ['API', 'SPA'],
-      projectLink: 'https://pixstock-official.vercel.app/'
-    },
-    {
-      imgSrc: '/images/project-3.jpg',
-      title: 'Recipe app',
-      tags: ['Development', 'API'],
-      projectLink: ''
-    },
-    {
-      imgSrc: '/images/project-4.jpg',
-      title: 'Real state website',
-      tags: ['Web-design', 'Development'],
-      projectLink: 'https://github.com/codewithsadee-org/wealthome'
-    },
-    {
-      imgSrc: '/images/project-5.jpg',
-      title: 'eCommerce website',
-      tags: ['eCommerce', 'Development'],
-      projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
-    },
-    {
-      imgSrc: '/images/project-6.jpg',
-      title: 'vCard Personal portfolio',
-      tags: ['Web-design', 'Development'],
-      projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
-    },
-  ];
+  //   {
+  //     imgSrc: '/images/project-2.jpg',
+  //     title: 'Free stock photo app',
+  //     tags: ['API', 'SPA'],
+  //     projectLink: 'https://pixstock-official.vercel.app/'
+  //   },
+  //   {
+  //     imgSrc: '/images/project-3.jpg',
+  //     title: 'Recipe app',
+  //     tags: ['Development', 'API'],
+  //     projectLink: ''
+  //   },
+  //   {
+  //     imgSrc: '/images/project-4.jpg',
+  //     title: 'Real state website',
+  //     tags: ['Web-design', 'Development'],
+  //     projectLink: 'https://github.com/codewithsadee-org/wealthome'
+  //   },
+  //   {
+  //     imgSrc: '/images/project-5.jpg',
+  //     title: 'eCommerce website',
+  //     tags: ['eCommerce', 'Development'],
+  //     projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
+  //   },
+  //   {
+  //     imgSrc: '/images/project-6.jpg',
+  //     title: 'vCard Personal portfolio',
+  //     tags: ['Web-design', 'Development'],
+  //     projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
+  //   },
+   ];
 
 export const Work = () => {
   return (
