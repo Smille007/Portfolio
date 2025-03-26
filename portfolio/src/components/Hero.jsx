4545aa606a012373cfa6365fb15dd1b6d13c63b1
Smille007 <smille007@gmai.com>
2025-03-26 17:07:36 -0400

    commit

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
           <div className="flex items-center gap-3">
             <figure className="img-box w-9 h-9 rounded-lg">
               <img
-                src="/public/images/avatar-1.JPG"
+                src="/images/avatar-1.JPG"
                 width={40}
                 height={40}
                 alt="Daryna Diaz"
