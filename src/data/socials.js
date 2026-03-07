3ebe16b19397514ab7d18a62756e845a777bbe41
Smille007 <smille007@gmai.com>
2026-03-07 12:57:55 -0500

    contact me fixed

---
+++ src/data/socials.js
@@ -0,0 +1,22 @@
+export const socials = [
+    {
+      label: 'GitHub',
+      href: 'https://www.github.com/smille007'
+    },
+    {
+      label: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/daryna-v-17469169/'
+    },
+    {
+      label: 'Twitter X',
+      href: 'https://x.com/TimothyDSnyder'
+    },
+    {
+      label: 'Instagram',
+      href: 'https://www.instagram.com/smille007'
+    },
+    {
+      label: 'Portfolio',
+      href: 'https://daryna-diaz.netlify.app/'
+    }
+  ];
